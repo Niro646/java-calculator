a6a29accbca59cf34936f42346bd8541f48d5646
Niro646 <nir602@gmail.com>
2023-03-27 18:21:54 +0300

    final commit

---
+++ app/src/main/java/com/example/myfirstprojecthit/MainActivity_second.java
@@ -0,0 +1,14 @@
+package com.example.myfirstprojecthit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity_second extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_second);
+    }
+}
